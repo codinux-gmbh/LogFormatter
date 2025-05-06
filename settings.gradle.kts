967d8e7dfcc335d39075ee53572ad44420d65aef
dankito <git@dankito.net>
2025-05-06 23:47:51 +0200

    Centralized plugin configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,19 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    plugins {
+        kotlin("multiplatform") version kotlinVersion
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+    }
+}
+
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
