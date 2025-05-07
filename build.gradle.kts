20124b2fdc357aa8080ce53d23e59b13a5602487
dankito <git@dankito.net>
2025-05-08 00:49:27 +0200

    Fixed repository url

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ allprojects {
     version = "1.0.0-SNAPSHOT"
 
 
-    ext["sourceCodeRepositoryBaseUrl"] = "github.com/codinux/LogUtils"
+    ext["sourceCodeRepositoryBaseUrl"] = "github.com/codinux-gmbh/LogUtils"
 
     ext["projectDescription"] = "Utils common to loggers like stacktrace shortener and class name abbreviator"
 }
