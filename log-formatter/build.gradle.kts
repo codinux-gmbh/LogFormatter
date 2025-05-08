267c4b888c0cb8625eec6949c309f92662ddc41c
dankito <git@dankito.net>
2025-05-09 01:02:47 +0200

    Added Gradle scripts to publish library

--- log-formatter/build.gradle.kts
+++ log-formatter/build.gradle.kts
@@ -108,3 +108,8 @@ kotlin {
         }
     }
 }
+
+
+if (File(projectDir, "../gradle/scripts/publish-codinux.gradle.kts").exists()) {
+    apply(from = "../gradle/scripts/publish-codinux.gradle.kts")
+}
