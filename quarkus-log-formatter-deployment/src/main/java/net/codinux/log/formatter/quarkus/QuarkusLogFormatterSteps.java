dba03c2f48749d25092ae817711f67e8dfecb055
dankito <git@dankito.net>
2025-05-15 01:24:42 +0200

    Added deployment steps so that QuarkusLogFormatter gets added to Quarkus extensions

---
+++ quarkus-log-formatter-deployment/src/main/java/net/codinux/log/formatter/quarkus/QuarkusLogFormatterSteps.java
@@ -0,0 +1,22 @@
+package net.codinux.log.formatter.quarkus;
+
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.logging.LoggingSetupBuildItem;
+import io.quarkus.runtime.RuntimeValue;
+import net.codinux.log.formatter.quarkus.config.QuarkusLogFormatterConfig;
+
+import java.util.Optional;
+import java.util.logging.Handler;
+
+public class QuarkusLogFormatterSteps {
+
+    @BuildStep
+    @Record(ExecutionTime.RUNTIME_INIT)
+    // LoggingSetupBuildItem is required so that this logic is run after log handlers have been fully set up
+    public void setUpLogAppender(QuarkusLogFormatterRecorder recorder, QuarkusLogFormatterConfig config, LoggingSetupBuildItem loggingSetup) {
+        RuntimeValue<Optional<Handler>> modifiedHandler = recorder.initializeLogFormatter(config);
+    }
+
+}
