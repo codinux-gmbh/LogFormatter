60e80fb2be74d0f13643d8fa04127cb97da9ef40
dankito <git@dankito.net>
2025-05-15 15:25:26 +0200

    Added documentation for package name

--- log-formatter/src/commonMain/kotlin/net/codinux/log/classname/ClassNameComponents.kt
+++ log-formatter/src/commonMain/kotlin/net/codinux/log/classname/ClassNameComponents.kt
@@ -2,12 +2,20 @@ package net.codinux.log.classname
 
 data class ClassNameComponents(
     val className: String,
+
+    /**
+     * The package in which this class is declared in.
+     *
+     * Not available in JavaScript and WASM and only a guess in Native.
+     */
     val packageName: String? = null,
+
     /**
      * For inner, local, anonymous classes and lambdas: The outmost top level class
      * that contains this class (if this information is available).
      */
     val declaringClass: String? = null
+
 ) {
     val packageNamePrefix: String = packageName?.let { "$it." } ?: ""
 
