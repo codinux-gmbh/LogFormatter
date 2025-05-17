3121b35aac09592804173ada3ec3b3d993e9e18f
dankito <git@dankito.net>
2025-05-17 20:35:08 +0200

    Added a task to automatically update version in poms and if it's not a SNAPSHOT also in README

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,3 +24,39 @@ allprojects {
 
     ext["projectDescription"] = "Utils common to loggers like stacktrace shortener and class name abbreviator"
 }
+
+
+
+gradle.taskGraph.whenReady {
+    setVersion(project.version.toString())
+}
+
+fun setVersion(version: String) {
+    val projectDir = project.projectDir
+
+    val parentPomVersionRegex = Regex("^    <version>[\\dSNAPSHOT.-]+</version>", RegexOption.MULTILINE)
+    val childPomVersionRegex = Regex("^        <version>[\\dSNAPSHOT.-]+</version>", RegexOption.MULTILINE)
+
+    val parentPomFile = projectDir.resolve("pom.xml")
+    val parentPomText = parentPomFile.readText()
+    val parentPomTextUpdated = parentPomText.replaceFirst(parentPomVersionRegex, "    <version>$version</version>")
+    parentPomFile.writeText(parentPomTextUpdated)
+
+    listOf(projectDir.resolve("quarkus-log-formatter-deployment"), projectDir.resolve("quarkus-log-formatter-runtime"))
+        .map { it.resolve("pom.xml") }
+        .forEach { childPomFile ->
+            val childPomText = childPomFile.readText()
+            val childPomTextUpdated = childPomText.replaceFirst(childPomVersionRegex, "        <version>$version</version>")
+            childPomFile.writeText(childPomTextUpdated)
+        }
+
+
+    if (version.endsWith("-SNAPSHOT") == false) {
+        val readme = file("README.md")
+
+        val updatedReadmeContent = readme.readText()
+            .replace(Regex("""(implementation\("net\.codinux\.log:log-formatter:)[^"]*""""), "$1$version\"")
+            .replace(Regex("""(^\s*<version>)[\d.]+(<\/version>$)""", RegexOption.MULTILINE), "$1$version$2")
+        readme.writeText(updatedReadmeContent)
+    }
+}
