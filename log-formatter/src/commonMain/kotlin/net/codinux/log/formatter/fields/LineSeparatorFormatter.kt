e26a9881a1e7779fea8d7cbe98bbcafd0ad5e72c
dankito <git@dankito.net>
2025-05-21 06:11:32 +0200

    Converted LineSeparatorFormatter to FieldFormatter

--- log-formatter/src/commonMain/kotlin/net/codinux/log/formatter/fields/LineSeparatorFormatter.kt
+++ log-formatter/src/commonMain/kotlin/net/codinux/log/formatter/fields/LineSeparatorFormatter.kt
@@ -1,12 +1,17 @@
 package net.codinux.log.formatter.fields
 
 import net.codinux.kotlin.text.LineSeparator
+import net.codinux.log.LogEvent
 
 open class LineSeparatorFormatter(
-    lineSeparator: String = LineSeparator.System
-) : LiteralFormatter(lineSeparator) {
+    protected open val lineSeparator: String = LineSeparator.System,
+    format: FieldFormat? = null
+) : FieldFormatter(format) {
 
-    override fun toString() = "Line separator: ${literal.map { when (it) {
+    override fun getField(event: LogEvent): String = lineSeparator
+
+
+    override fun toString() = "Line separator: ${lineSeparator.map { when (it) {
             '\n' -> "\\n"
             '\r' -> "\\r"
             else -> it.toString()
