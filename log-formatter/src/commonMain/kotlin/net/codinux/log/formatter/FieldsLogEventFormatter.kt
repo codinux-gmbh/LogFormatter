dbe463e178f955d4e7246095178453d9933ad2ae
dankito <git@dankito.net>
2025-05-21 03:08:37 +0200

    Added DefaultFields to FieldsLogEventFormatter

--- log-formatter/src/commonMain/kotlin/net/codinux/log/formatter/FieldsLogEventFormatter.kt
+++ log-formatter/src/commonMain/kotlin/net/codinux/log/formatter/FieldsLogEventFormatter.kt
@@ -1,11 +1,26 @@
 package net.codinux.log.formatter
 
 import net.codinux.log.LogEvent
-import net.codinux.log.formatter.fields.LogLinePartFormatter
+import net.codinux.log.formatter.fields.*
 
 open class FieldsLogEventFormatter(
-    protected open val fields: List<LogLinePartFormatter>
+    protected open val fields: List<LogLinePartFormatter> = DefaultFields
 ) : LogEventFormatter {
+
+    companion object {
+        private val DefaultFields = listOf(
+            LogLevelFormatter(),
+            WhitespaceFormatter,
+            LoggerNameFormatter(),
+            LiteralFormatter(" ["),
+            ThreadNameFormatter(),
+            LiteralFormatter("] "),
+            MessageFormatter(),
+            LineSeparatorFormatter(),
+            ThrowableFormatter()
+        )
+    }
+
 
     override fun formatEvent(event: LogEvent): String = buildString {
         fields.forEach { field ->
--- log-formatter/src/commonTest/kotlin/net/codinux/log/formatter/FieldsLogEventFormatterTest.kt
+++ log-formatter/src/commonTest/kotlin/net/codinux/log/formatter/FieldsLogEventFormatterTest.kt
@@ -5,32 +5,16 @@ import assertk.assertions.*
 import net.codinux.kotlin.text.LineSeparator
 import net.codinux.log.LogEvent
 import net.codinux.log.LogLevel
-import net.codinux.log.formatter.fields.*
 import kotlin.test.Test
 
 class FieldsLogEventFormatterTest {
-
-    companion object {
-        private val DefaultFields = listOf(
-            LogLevelFormatter(),
-            WhitespaceFormatter,
-            LoggerNameFormatter(),
-            LiteralFormatter(" ["),
-            ThreadNameFormatter(),
-            LiteralFormatter("] "),
-            MessageFormatter(),
-            LineSeparatorFormatter(),
-            ThrowableFormatter()
-        )
-    }
-
 
     @Test
     fun defaultFields_WithoutThrowable() {
         val event = LogEvent(LogLevel.Info, "Just a test message", "UserService", "main")
 
 
-        val result = FieldsLogEventFormatter(DefaultFields).formatEvent(event)
+        val result = FieldsLogEventFormatter().formatEvent(event)
 
 
         assertThat(result).isEqualTo("Info UserService [main] Just a test message${LineSeparator.System}")
@@ -41,7 +25,7 @@ class FieldsLogEventFormatterTest {
         val event = LogEvent(LogLevel.Info, "Just a test message", "UserService", "main", Throwable("No animals have been harmed"))
 
 
-        val result = FieldsLogEventFormatter(DefaultFields).formatEvent(event)
+        val result = FieldsLogEventFormatter().formatEvent(event)
 
         val lines = result.lines()
         assertThat(lines.size).isGreaterThan(2)
