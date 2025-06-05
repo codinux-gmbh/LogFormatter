45f49659c57a4328d41fffff3269130fbba19011
dankito <git@dankito.net>
2025-06-05 16:05:46 +0200

    Added timeZone to DateTimeFormatter, but only supported value for now is "UTC"

--- log-formatter/src/commonMain/kotlin/net/codinux/log/formatter/fields/DateTimeFormatter.kt
+++ log-formatter/src/commonMain/kotlin/net/codinux/log/formatter/fields/DateTimeFormatter.kt
@@ -1,11 +1,13 @@
 package net.codinux.log.formatter.fields
 
 import net.codinux.log.LogEvent
+import net.codinux.log.error.ErrorReporter
 import net.dankito.datetime.format.pattern.DateTimeComponentFormatter
 
 open class DateTimeFormatter(
     format: FieldFormat? = null,
     protected val dateTimePattern: String? = null,
+    protected val timeZone: String? = null,
     protected val formatter: DateTimeComponentFormatter = DateTimeComponentFormatter.Default
 ) : FieldFormatter(format) {
 
@@ -18,7 +20,14 @@ open class DateTimeFormatter(
         if (event.timestamp == null) {
             FieldValueNotAvailable
         } else {
-            val dateTime = event.timestamp!!.toLocalDateTimeAtSystemTimeZone()
+            val timestamp = event.timestamp!!
+            val dateTime = if ("UTC".equals(timeZone, ignoreCase = true)) timestamp.toLocalDateTimeAtUtc()
+                            else if (timeZone == null) timestamp.toLocalDateTimeAtSystemTimeZone()
+                            else {
+                                ErrorReporter.reportError("Time zone for date time format has been set to '$timeZone', " +
+                                        "but the only currently supported time zone value is 'UTC'. Falling back to system time zone")
+                                timestamp.toLocalDateTimeAtSystemTimeZone()
+                            }
 
             val pattern = dateTimePattern ?: DefaultDateTimeFormat
 
