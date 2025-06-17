c2cbdd0f86c68b6dcb475bd4dbf0c36108a8a888
dankito <git@dankito.net>
2025-06-17 04:14:12 +0200

    Released version 1.7.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ allprojects {
     }
 
     group = "net.codinux.log"
-    version = "1.7.3-SNAPSHOT"
+    version = "1.7.3"
 
 
     ext["sourceCodeRepositoryBaseUrl"] = "github.com/codinux-gmbh/LogFormatter"
